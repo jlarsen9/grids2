babae7897e3caa9b44551ca81a2adc72cc5193a7
Jenny Larsen <jennylarsen91@gmail.com>
2019-03-09 12:15:57 -0800

    made randomized word part

--- script.js
+++ script.js
@@ -1,16 +1,30 @@
+//display-only text box
+
+var textBox = document.getElementById("text-box")
+textBox.innerHTML = "game"
+
+
+
+//grid js
+
 function createButtonRow(numButtons) {
     var row = ""
+    var letters = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
     for (var i = 1; i <= numButtons; i++) {
-        button = "<button>" + i + "</button>"
+        var letterToDisplay = letters[(Math.floor(Math.random() * 26))]
+        button = "<button>" + letterToDisplay + "</button>"
         row = row + button
     }
-    var contentDiv = document.getElementById("jenny-grid")
-    contentDiv.innerHTML += row + "<br></br>"
+    var jennyGrid = document.getElementById("jenny-grid")
+    jennyGrid.innerHTML += row + "<br></br>"
 }
 
 var numRows = 3
-var numColumns = 3
+var numColumns = 4
+createButtonRow(numColumns)
+
 
 for (var i = 1; i <= numRows; i++) {
     createButtonRow(numColumns)
 }
+
